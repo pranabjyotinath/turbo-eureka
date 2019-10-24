83c8e0a99a4a247a4103c79218b250463217510d
Pranabjyotin <neilshankarnath@gmail.com>
2019-10-24 16:53:15 +0100

    Added meme command

--- commands/fun/meme.js
+++ commands/fun/meme.js
@@ -6,7 +6,7 @@ module.exports = {
     category: "fun",
     description: "Sends an epic meme!",
     run: async (client, message, args) => {
-        const subReddits = ["dankmeme", "meme", "me_irl"]
+        const subReddits = ["dankmeme", "meme"]
         const random = subReddits[Math.floor(Math.random() * subReddits.length)]
 
         const img = await randomPuppy(random)
