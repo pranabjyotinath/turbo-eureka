e13922a3e2449ad1db31df8fe9da68d6810b0496
Pranabjyotin <neilshankarnath@gmail.com>
2019-10-26 13:06:57 +0100

    Kill Command

--- commands/fun/kill.js
+++ commands/fun/kill.js
@@ -8,6 +8,11 @@ module.exports = {
 run: async (client, message, args) => {
     // Get a member from mention, id, or username
     let person = getMember(message, args[0]);
-    console.log(person + message)
+
+    const embed = new RichEmbed()
+            .setColor("#ffb6c1")
+            .addField(`A test ${person}`);
+   
+    message.channel.send(embed);
 }
 }
