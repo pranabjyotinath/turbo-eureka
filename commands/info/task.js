a465b7940640dc7d4abf651368643c11e4ea4a44
Pranabjyotin <neilshankarnath@gmail.com>
2019-10-26 14:20:26 +0100

    Added a tasks command for devlopment only

---
+++ commands/info/task.js
@@ -0,0 +1,17 @@
+const {richEmbed} = require('discord.js')
+
+module.exports = {
+    name: "tasks",
+    description: "Just a development Command",
+    run: async (client, message, args) => {
+        const embed = new richEmbed()
+            .setTitle('Next Commands to Be made')
+            .addField('1. Music Command')
+            .addField('2. Clear Command')
+            .addField('3. Joke Command')
+            .addField('4. Prefix Command')
+            
+        message.channel.send(embed);
+    }
+    
+}
