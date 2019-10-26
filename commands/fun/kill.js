7aae2de6e8b98f9d5ffc8855e6f1b5c326116e51
Pranabjyotin <neilshankarnath@gmail.com>
2019-10-26 14:13:45 +0100

    Improved Kill COmmand

--- commands/fun/kill.js
+++ commands/fun/kill.js
@@ -10,7 +10,16 @@ run: async (client, message, args) => {
     // Get a member from mention, id, or username
     let dead = getMember(message, args[0]);
 
-    const dialog = Array(`**@${dead.displayName}** died due to diarhea because of **${message.member.displayName}**`)
+    const dialog = Array(
+        `**${message.member.displayName}** killed **${dead.displayName}** by singing a song`,
+        `**${dead.displayName}** died due to Diarrhea because of **${message.member.displayName}**`,
+        `**${dead.displayName}** died due to **${message.member.displayName}**'s Kung Fu Moves`,
+        `**${dead.displayName}** was killed by **${message.member.displayName}** the zombie`,
+        `**${dead.displayName}** died by hearing **${message.member.displayName}**'s Joke`,
+        `**${dead.displayName}** was killed by **${message.member.displayName}** for no reason`,
+        `**${dead.displayName}** died of a bomb placed by **${message.member.displayName}**`,
+        `**${dead.displayName}** died because of **${message.member.displayName}**'s food`
+    )
 
     const randomElement = dialog[Math.floor(Math.random()*dialog.length)]
 
