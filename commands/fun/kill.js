1dc3757d62f0d8addf493697b32366b42e63e0bb
Pranabjyotin <neilshankarnath@gmail.com>
2019-10-26 13:09:11 +0100

    Kill Command

--- commands/fun/kill.js
+++ commands/fun/kill.js
@@ -1,19 +1,37 @@
-const { getMember } = require("../../function.js");
 const { RichEmbed } = require("discord.js");
+const { getMember } = require("../../function.js");
 
 module.exports = {
     name: "kill",
     category: "fun",
+    description: "Kill someone you don't like",
     usage: "[mention | id | username]",
+    run: async (client, message, args) => {
+        // Get a member from mention, id, or username
+        let person = getMember(message, args[0]);
 
-run: async (client, message, args) => {
-    // Get a member from mention, id, or username
-    let person = getMember(message, args[0]);
+        // If no person is found
+        // It's going to default to the author
+        // And we don't want to love ourself in this command
+        // So we filter out our ID from the server members
+        // And get a random person from that collection
+        if (!person || message.author.id === person.id) {
+            person = message.guild.members
+                .filter(m => m.id !== message.author.id)
+                .random();
+        }
 
-    const embed = new RichEmbed()
+        // love is the percentage
+        // loveIndex is a number from 0 to 10, based on that love variable
+        const love = Math.random() * 100;
+        const loveIndex = Math.floor(love / 10);
+        const loveLevel = "💖".repeat(loveIndex) + "💔".repeat(10 - loveIndex);
+
+        const embed = new RichEmbed()
             .setColor("#ffb6c1")
-            .addField(`A test ${person}`);
-   
-    message.channel.send(embed);
-}
+            .addField(`☁ **${person.displayName}** loves **${message.member.displayName}** this much:`,
+            `💟 ${Math.floor(love)}%\n\n${loveLevel}`);
+
+        message.channel.send(embed);
+    }
 }
