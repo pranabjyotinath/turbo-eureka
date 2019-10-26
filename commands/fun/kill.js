8a255acefc101a692d579f8813c8c27847d9b859
Pranabjyotin <neilshankarnath@gmail.com>
2019-10-26 15:27:59 +0100

    Changed somethings

--- commands/fun/kill.js
+++ commands/fun/kill.js
@@ -11,15 +11,15 @@ run: async (client, message, args) => {
     let dead = getMember(message, args[0]);
 
     const dialog = Array(
-        // `**${message.member.displayName}** killed **${dead.displayName}** by singing a song`,
-        // `**${dead.displayName}** died due to Diarrhea because of **${message.member.displayName}**`,
-        // `**${dead.displayName}** died due to **${message.member.displayName}**'s Kung Fu Moves`,
-        // `**${dead.displayName}** was killed by **${message.member.displayName}** the zombie`,
-        // `**${dead.displayName}** died by hearing **${message.member.displayName}**'s Joke`,
-        // `**${dead.displayName}** was killed by **${message.member.displayName}** for no reason`,
-        // `**${dead.displayName}** died of a bomb placed by **${message.member.displayName}**`,
-        // `**${dead.displayName}** died because of **${message.member.displayName}**'s food`,
-        // `**${dead.displayName}** died by dabbing hard`,
+        `**${message.member.displayName}** killed **${dead.displayName}** by singing a song`,
+        `**${dead.displayName}** died due to Diarrhea because of **${message.member.displayName}**`,
+        `**${dead.displayName}** died due to **${message.member.displayName}**'s Kung Fu Moves`,
+        `**${dead.displayName}** was killed by **${message.member.displayName}** the zombie`,
+        `**${dead.displayName}** died by hearing **${message.member.displayName}**'s Joke`,
+        `**${dead.displayName}** was killed by **${message.member.displayName}** for no reason`,
+        `**${dead.displayName}** died of a bomb placed by **${message.member.displayName}**`,
+        `**${dead.displayName}** died because of **${message.member.displayName}**'s food`,
+        `**${dead.displayName}** died by dabbing hard`,
         `**${dead.displayName}** spammed L too much that he got burnt`
     )
 
